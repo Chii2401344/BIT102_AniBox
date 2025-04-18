0a11c64f59d1e6f3f1e92b6e3e150d88d1214a03
nakedpeanuts <b2401411@helplive.edu.my>
2025-04-19 05:40:43 +0800

    added navbar php
    - changed romance genre color
    - changed review settings button color

--- animes/anime.php
+++ animes/anime.php
@@ -1,5 +1,6 @@
 <?php
-require('connect.php'); // Adjust path if needed
+require 'connect.php';
+include '../user/navbar.php';
 
 // Step 1: Get ID from URL and validate it
 if (isset($_GET['id']) && is_numeric($_GET['id'])) {
@@ -31,7 +32,7 @@ $banner_img = $anime['Banner_Img'];
 
 $color = match (strtolower($genre)) {
     'comedy' => 'orange',
-    'romance' => 'deeppink',
+    'romance' => 'lightpink',
     'action' => 'red',
     default => 'gray'
 };
@@ -68,53 +69,6 @@ body {
     background-position: top;
 }
 </style>
-
-    <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="../user/user-home.html">AniBox</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
-                <ul class="navbar-nav mx-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="../user/user-home.html">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../user/user-home.html#browse">Browse</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../user/user-profile-mybox.html">My Box</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="../user/user-profile-about.html">Profile</a>
-                    </li>
-                </ul>
-                <hr class="d-lg-none my-2 text-dark-50">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a href="../user/user-settings.html" class="d-none d-lg-block me-3">
-                            <i class="fa-solid fa-gear"></i>
-                        </a>
-                        <a class="nav-link d-block d-lg-none" href="../user/user-settings.html">Settings</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" id="logoutDesktop" class="d-none d-lg-block me-3">
-                            <i class="fas fa-sign-out-alt"></i>
-                        </a>
-                        <a href="#" id="logoutMobile" class="nav-link d-block d-lg-none">Logout</a>
-                    </li>
-                </ul>
-
-                <!-- Profile Picture -->
-                <a href="../user/user-profile-about.html" class="card-container d-none d-lg-block">
-                    <img src="../assets/img/pfp3.jpg" alt="profile" class="user-icon">
-                </a>
-
-            </div>
-        </div>
-    </nav>
 
     <!-- Container for Anime Details and Add to Box Button Section -->
     <div class="container1">
